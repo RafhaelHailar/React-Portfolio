486c5f7938e3ade3f196186c1fdebdbed8bf6429
Rafhael <rafhael4hailar@gmail.com>
2023-11-28 20:03:24 +0800

    WEWEWEWEW redo the posts items

--- src/posts.js
+++ src/posts.js
@@ -1,20 +1,20 @@
 const posts = {
     "1": {
         "id": 1,
-        "date": "September 2, 2020",
-        "description": " I am handsome man. Get answers to frequently asked questions about the Poetry Foundation’s online archive, including permissions and suggestions.Get answers to frequently asked questions about the Poetry Foundation’s online archive, including permissions and suggestions.",
+        "date": "July 29, 2023",
+        "description": "Don't be yourself if you don't know who you are yet, although you will never know who you are because you are always changing, so figure out who you want to change into then who you wanted to be, dont get swallowed by some fallacies. \n Image Credits: [https://visit.museum.wa.gov.au/maritime/sea-monsters-prehistoric-ocean-predators](Sea Monsters: Prehistoric Ocean Predators)",
         "media": "https://visit.museum.wa.gov.au/sites/default/files/styles/large_/public/2022-12/wamaritimemuseum-seamonsters-header-2000px.jpg?h=5f3ce5bb&itok=NV4bWpD6"
     },
     "2": {
         "id": 2,
-        "date": "November 1, 2023",
-        "description": "Trying to make a horror game in pure js",
+        "date": "July 30, 2023",
+        "description": "Follow my github if you want to see some cool things hehehe\n [https://github.com/RafhaelHailar](Rafhael Hailar Github Account) ",
         "media": ""
     },
     "3": {
         "id": 3,
-        "date": "November 8, 2023",
-        "description": "Boreeddd",
+        "date": "August 2, 2023",
+        "description": "Look into your eyes\n Image Credits: [https://pics.alphacoders.com/pictures/view/1338](Bored Fish)",
         "media": "https://i.imgur.com/4VAD9Pw.jpeg"
     },
     "4": {
@@ -28,7 +28,27 @@ const posts = {
         "date": "October 29, 2023",
         "description": "Develop a simple website, A rental website where user can rent a girlfriend.\nMade with HTML,CSS and Materialize.\n Try it here: [https://cwd-b-11-rafhaelhailar.netlify.app/](RentGal!) \n Source Code here: [https://github.com/RafhaelHailar/RentGal-_SAMPLE](RentGal! Source Code)",
         "media": "https://raw.githubusercontent.com/RafhaelHailar/RentGal-_SAMPLE/master/images/current_look.jpeg"
-    }
+    },
+    "6": {
+        "id": 6,
+        "date": "November 1, 2023",
+        "description": "Sea this? (PUN INTENDED)\n\nImage Credits: [https://climate.nasa.gov/explore/ask-nasa-climate/2990/sea-level-101-what-determines-the-level-of-the-sea/](Sea Level 101: What Determines the Level of the Sea?)",
+        "media": "https://raw.githubusercontent.com/RafhaelHailar/React-Portfolio/master/src/images/image-sea.jpg"
+    }, 
+    "7": {
+        "id": 7,
+        "date": "November 1, 2023",
+        "description": "I am going to do some part of my portfolio first then create new website. PRACTICE PRACTICE PRACTICE",
+        "media": ""
+    },
+    "8": {
+        "id": 8,
+        "date": "November 2, 2023",
+        "description": "Making a horror game in pure js",
+        "media": ""
+    },
+
+    
 }
 const total = Object.keys(posts).length;
 
